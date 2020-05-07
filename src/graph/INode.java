14c095b635f4e92aabb7e6494594b4b892ba1ca7
Aakriti <>
2020-05-06 22:45:03 -0500

    Update INode.java

--- src/graph/INode.java
+++ src/graph/INode.java
@@ -1,22 +1,27 @@
 package graph;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collection;
 
 public interface INode
 {
-    String getName();
+    String getName(); //method getname which might get the name
     
-    Collection<INode> getNeighbors();
+    Collection<INode> getNeighbors(); //collection type...hm...getsNeighbours 
     
-    void addDirectedEdgeToNode(INode neighbor, int weight);
+    void addDirectedEdgeToNode(INode neighbor, int weight); //no public/private...adds directed edges to node
     
-    void addUndirectedEdgeToNode(INode neighbor, int weight);
+    void addUndirectedEdgeToNode(INode neighbor, int weight); //adds undirected edges to node
     
-    void removeDirectedEdgeToNode(INode neighbor);
+    void removeDirectedEdgeToNode(INode neighbor); //removes de
     
-    void removeUndirectedEdgeToNode(INode neightbor);
+    void removeUndirectedEdgeToNode(INode neightbor); //removes ue
     
-    boolean hasEdge(INode node);
+    boolean hasEdge(INode node); //to check if inode has edge or not
     
-    int getWeight(INode node);
+    int getWeight(INode node); //gets the weight of the node
+    
+    
 }
+    	    
